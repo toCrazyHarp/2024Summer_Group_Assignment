f23f64f6fa1ea77c993d8750a0e6c5d2f1ed511d
Crazy_harp <av3709035@163.com>
2024-07-11 08:08:25 +0800

    数据库初始化

--- Schedule_management/Info/models.py
+++ Schedule_management/Info/models.py
@@ -4,10 +4,17 @@ from django.db import models
 
 class Tasks(models.Model):
   """短期任务表"""
-
+  title = models.CharField(max_length=100)
+  start_time = models.DateTimeField()
 class longperiodtasks(models.Model):
   """长期任务表"""
+  title = models.CharField(max_length=100)
+  start_time = models.DateTimeField()
+  end_time = models.DateTimeField()
+
 
 class routinetasks(models.Model):
   """周期性任务表"""
-  
+  title = models.CharField(max_length=100)
+  mode = models.CharField(max_length=1)
+  day = models.IntegerField
